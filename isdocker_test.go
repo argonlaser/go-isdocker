203f3c97a8a256fa814eff4eb452c251ff1fbb15
Venkata krishna Sundararajan <vkvenkat94@gmail.com>
2017-03-21 22:11:47 +0530

    adds tests for docker and non docker mode. edits travis.yml.
    Signed-off-by: Venkata krishna Sundararajan <vkvenkat94@gmail.com>

--- isdocker_test.go
+++ isdocker_test.go
@@ -4,9 +4,16 @@ import (
 	"testing"
 )
 
-func TestIsDocker(t *testing.T) {
+func TestIsDockerProcess(t *testing.T) {
 	isdocker := IsDocker()
 	if isdocker == false {
 		t.Errorf("process is in docker mode, but shows non-docker mode !!!")
+	}
+}
+
+func TestIsNonDockerProcess(t *testing.T) {
+	isdocker := IsDocker()
+	if isdocker == true {
+		t.Errorf("process is in non-docker mode, but shows docker mode !!!")
 	}
 }
